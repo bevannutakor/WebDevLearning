8132219414c9da75bf91c4d11199517481abe1fd
bevannutakor <bevannutakor@icloud.com>
2022-05-10 11:39:11 -0700

    initial setup

---
+++ cv-project/src/components/Information.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import './App.css';
+
+class Information extends Component{
+    constructor(props){
+      super(props)
+    }
+    
+  }
+  
+  export default Information;
