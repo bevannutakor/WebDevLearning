32eb904c276844f121794414a0426003406e875b
bevannutakor <bevannutakor@icloud.com>
2022-06-06 11:36:28 -0700

    Testing practice

--- router-practice/src/App.js
+++ router-practice/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
+//testing purposes
 const App = () => {
+  const [heading, setHeading] = useState("Magnificent Mondays");
+
+  const clickHandler = () => {
+    setHeading("radical rhinos")
+  }
   return (
-    <div>
-      <h1>Hello from App</h1>
-    </div>
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
   );
 }
 
